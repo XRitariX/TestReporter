9755bfa1258f1427a2d18c552e83693329d6c7b2
Fikeery <sankaxaker@gmail.com>
2026-05-23 07:27:12 +0300

    Проба пера

---
+++ Servise/Filtrations/FilterRept.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestReporter.Models;
+
+namespace TestReporter.Servise.Filtrations
+{
+    internal class FilterRept
+    {
+        public IEnumerable<StudentAnswer> ApplyFilters
+            (IEnumerable<StudentAnswer> source,
+            FilterOptions filters)
+        {
+            if (source == null) return Enumerable.Empty<StudentAnswer>();
+            if (filters == null) return source;
+            var query = source.AsQueryable();
+            
+            if (filters.StartDate.HasValue) query = query.Where(a => a.TestDate >= filters.StartDate.Value);
+            if (filters.EndDate.HasValue) query = query.Where(a => a.TestDate <= filters.EndDate.Value);
+           
+            if (filters.SelectedThemes != null && filters.SelectedThemes.Any())
+                query = query.Where(a => filters.SelectedThemes.Contains(a.TopicName));
+            
+            if (filters.SelectedGroups != null && filters.SelectedGroups.Any())
+                query = query.Where(a => !string.IsNullOrEmpty(a.GroupName) && filters.SelectedGroups.Contains(a.GroupName));
+            
+            if (filters.SelectedStudents != null && filters.SelectedStudents.Any())
+                query = query.Where(a => filters.SelectedStudents.Contains(a.StudentName));
+            return query.ToList();
+        }
+    }
+}
