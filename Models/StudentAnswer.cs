edbd2ab3f8d85499e6f287555c52d35b63dcaa59
Fikeery <sankaxaker@gmail.com>
2026-06-02 20:43:13 +0300

    Приложение запускается

--- Models/StudentAnswer.cs
+++ Models/StudentAnswer.cs
@@ -8,6 +8,10 @@ namespace TestReporter.Models
 {
     public class StudentAnswer
     {
+        public string StudentName { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
+        public string TopicName { get; set; } = string.Empty;
+        public DateTime? TestDate { get; set; }
         public string QuestionName { get; set; } = string.Empty;
         public string? AnswerText { get; set; }
         public int? Score { get; set; }
